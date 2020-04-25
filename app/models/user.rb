13c2a8739d6ee5e8ffc61f068a2d5075d7dc7376
Ulf Möhring <ulf@moehring.me>
2020-04-25 23:05:43 +0200

    Make stylsheet optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Associations
   has_many :pages, -> { order('position') }
   has_many :providers, -> { order('type') }
-  belongs_to :stylesheet, foreign_key: 'page_id', class_name: 'Page'
+  belongs_to :stylesheet, foreign_key: 'page_id', class_name: 'Page', optional: true
 
   # Include devise modules
   devise :database_authenticatable, :registerable, :confirmable, :validatable, :confirmable, :lockable, :rememberable
